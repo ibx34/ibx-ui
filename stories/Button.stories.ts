b27251535d10db51fc35be5262ee19c6171adbc9
Shadow Wizurd Money Gang Leader <Guamjust@gmail.com>
2023-04-24 22:27:33 +0000

    yes

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -6,7 +6,9 @@ export default {
   component: Button,
   tags: ['autodocs'],
   argTypes: {
-    backgroundColor: { control: 'color' },
+    primary: { 
+      control: { type: 'switch' },
+     },
   },
 };
 
